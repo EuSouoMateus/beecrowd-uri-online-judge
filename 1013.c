76c7a193349e5330e180fd2c62f2664cf77b5892
Mateus Lopes <83896229+MateLopes@users.noreply.github.com>
2022-02-03 14:21:59 -0300

    1013 solved in C

---
+++ 1013.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c, y, x;
+    scanf("%d %d %d", &a, &b, &c);
+
+    y = (a + b + abs(a-b)) / 2;
+    x = (y + c + abs(y-c)) / 2;
+
+    printf("%d eh o maior\n", x);
+    return 0;
+}
