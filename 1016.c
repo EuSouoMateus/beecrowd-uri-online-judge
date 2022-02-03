c4d5fc831bbf3526ebf15a4ba7b856fb6b63e306
Mateus Lopes <83896229+MateLopes@users.noreply.github.com>
2022-02-03 18:33:51 -0300

    1016. solved in C

---
+++ 1016.c
@@ -0,0 +1,11 @@
+int main() { 
+    int num, horas, distanciar, minutos;
+    scanf("%d", &num);
+
+    horas = 60;
+    distanciar = 30;
+    minutos = (horas * num) / distanciar;
+
+    printf("%d minutos\n", minutos);
+    return 0;
+}
